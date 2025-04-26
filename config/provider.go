235d6b4b4ee73290f323983091f175bd95bb4df1
warjiang <1096409085@qq.com>
2025-04-26 17:54:10 +0800

    feat: init project
    Signed-off-by: warjiang <1096409085@qq.com>

--- config/provider.go
+++ config/provider.go
@@ -9,13 +9,12 @@ import (
 	_ "embed"
 
 	ujconfig "github.com/crossplane/upjet/pkg/config"
-
-	"github.com/upbound/upjet-provider-template/config/null"
+	// "github.com/warjiang/provider-volcengine-terraform/config/null"
 )
 
 const (
-	resourcePrefix = "template"
-	modulePath     = "github.com/upbound/upjet-provider-template"
+	resourcePrefix = "volcengine-terraform"
+	modulePath     = "github.com/warjiang/provider-volcengine-terraform"
 )
 
 //go:embed schema.json
@@ -27,7 +26,7 @@ var providerMetadata string
 // GetProvider returns provider configuration
 func GetProvider() *ujconfig.Provider {
 	pc := ujconfig.NewProvider([]byte(providerSchema), resourcePrefix, modulePath, []byte(providerMetadata),
-		ujconfig.WithRootGroup("template.upbound.io"),
+		ujconfig.WithRootGroup("crossplane.io"),
 		ujconfig.WithIncludeList(ExternalNameConfigured()),
 		ujconfig.WithFeaturesPackage("internal/features"),
 		ujconfig.WithDefaultResourceOptions(
@@ -36,7 +35,7 @@ func GetProvider() *ujconfig.Provider {
 
 	for _, configure := range []func(provider *ujconfig.Provider){
 		// add custom config functions
-		null.Configure,
+		// null.Configure,
 	} {
 		configure(pc)
 	}
